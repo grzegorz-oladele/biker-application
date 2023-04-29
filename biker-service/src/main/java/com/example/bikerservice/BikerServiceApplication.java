f709d7a9dc9468a8c7089b98ce64f2f3eeccdfb4
grzegorz_oladele <grzegorz.oladele@gmail.com>
2023-04-29 15:29:58 +0200

    Initializing project

---
+++ biker-service/src/main/java/com/example/bikerservice/BikerServiceApplication.java
@@ -0,0 +1,17 @@
+package com.example.bikerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+})
+public class BikerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikerServiceApplication.class, args);
+    }
+
+}
---
+++ biker-service/src/test/java/com/example/bikerservice/BikerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bikerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
